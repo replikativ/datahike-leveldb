4cfbeb0622f489afb7de0465a977537f1adceb03
Judith <j.massa@lambdaforge.io>
2019-12-19 17:02:40 +0100

    Fix version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.replikativ/datahike-leveldb "0.0.1-SNAPSHOT"
+(defproject io.replikativ/datahike-leveldb "0.1.0-SNAPSHOT"
   :description "Datahike with LevelDB as data storage."
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
